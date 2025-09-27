e800330ec5361b3035a2a31088566ecf5a52e4f3
Laura Torres <torresmurillolaurasofia01@gmail.com>
2025-09-27 16:12:37 -0500

    primera confirmación

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/procesar', methods=['POST'])
+def procesar():
+    nombre = request.form.get('nombre')
+    mensaje = f"¡Hola, {nombre}! Este saludo viene desde Python."
+    return render_template('index.html', mensaje=mensaje)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
